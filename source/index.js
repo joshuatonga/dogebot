7541a288217a6b79e0d7e71ab62e80528e33bfbc
Joshua Tonga <hellojoshuatonga@gmail.com>
2016-06-09 12:54:22 +0800

    Initial

---
+++ source/index.js
@@ -0,0 +1,49 @@
+//*************************************
+// Packages
+//*************************************
+
+// Slack SDK
+var Slack = require('@slack/client');
+var RTMClient = Slack.RtmClient;
+var CLIENT_EVENTS = Slack.CLIENT_EVENTS;
+var RTM_EVENTS = Slack.RTM_EVENTS;
+
+
+//*************************************
+// Global variables
+//*************************************
+
+var token = process.env.SLACK_TOKEN || '';
+
+
+
+
+
+
+// start the websocket
+var rtm = new RTMClient(token, {logLevel: 'debug'});
+rtm.start();
+
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+  console.log('[+] Dogebot is running... MUCH BOT! SO WOW!');
+});
+
+
+// Listen for messages in channel
+rtm.on(RTM_EVENTS.MESSAGE, function(data) {
+  console.log(data);
+
+  // Perform validation of the data
+  if (!data.text || data.subtype === 'bot_message')
+    return;
+
+  // Extract data from data
+  var message = data.text;
+  var channel = data.channel;
+
+
+  if (message == 'test bot') {
+    rtm.sendMessage('WOW!', channel);
+  }
+
+});
